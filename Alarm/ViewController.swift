aaf8517f61a04a41c3350f7eebbefb0f4c2bb59c
Nancy Vidales <nancy.vidales01@utrgv.edu>
2018-03-28 14:46:01 -0500

    Initial Commit

---
+++ Alarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alarm
+//
+//  Created by Nancy Vidales on 3/28/18.
+//  Copyright © 2018 Nancy Vidales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
